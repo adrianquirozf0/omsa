b415780b3e0830007424fd7ea618f30633b0c3f9
carlospolanco <guelo0508@gmail.com>
2015-02-25 06:22:16 -0800

    distancia y tiempo
    se añadio al codigo los calculos de distancia estimada y tiempo de
    llegada al codigo

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -17,25 +17,35 @@ angular
 		    v: '3.exp',
 		  });
 	}])
-	.controller("maincontroller", ['$geolocation', '$scope',function($geolocation,$scope){
+	.value("http_config",{
+		host:"",
+	})
+
+	.controller("maincontroller", ['$geolocation', '$scope',"uiGmapIsReady","http_config","$http",
+		function($geolocation,$scope,uiGmapIsReady,http_config,http){
 		 $geolocation.watchPosition({
             timeout: 60000,
             maximumAge: 0,
             enableHighAccuracy: true
+
         });
 
-        $scope.$watch(function() {
-        	return $geolocation.position.coords;
-        }, function (new_val) {
-        	console.log(new_val);
-        	scope.map.marker.latitude=new_val.Coordinates.latitude;
-        	scope.map.marker.latitude=new_val.Coordinates.longitude;
-        });
+		 $scope.paused = false;
+     $scope.duracion = "Calculando...";
+     $scope.distancia = "Calculando...";
 
-		$scope.map = {
+		 $scope.map = {
+		 	control: {},
 			marker: { 
-				latitude:18,
-				      longitude:-69,
+
+				 id: 0,
+		        coords: {
+		            latitude: 18.488044,
+		            longitude: -64.96300,
+		        },
+		        options:{
+		        	draggable:true
+		        }
 
 			},
 			zoom: 16, 
@@ -44,8 +54,81 @@ angular
 				longitude: -69.962271, 
 			},
 		};
+             $scope.$watch(function() {
+        	return $geolocation.position.coords;
+        }, function (new_val) {
+        	if (!new_val) {
+        		return;
+        	}
+        	
+        	console.log(new_val.latitude,new_val.longitude);
+        	$scope.map.marker.coords.latitude=new_val.latitude;
+        	$scope.map.marker.coords.longitude=new_val.longitude;
+        });
 
-		$scope.prueba=function(){
-			alert("vamos bien");
-		};
+
+        $scope.$watch(function() {
+        	return $scope.map.marker.coords;
+        }, function (new_val, old_val) {
+        	if (!new_val || new_val == old_val) {
+        		return;
+        	}
+        	if (!$scope.paused) {
+        		$scope.submit();
+        	}
+        }, true);
+
+           $scope.submit = function(){
+           	var url = http_config.host + "/fixes.json";
+           	var now = new Date();
+           	var myDate = now.getFullYear()+"-"+("0"+(now.getMonth()+1)).slice(-2)+"-"+("0"+now.getDate()).slice(-2)+" "+
+           	 ("0"+now.getHours()).slice(-2)+":"+("0"+now.getMinutes()).slice(-2)+":"+("0"+now.getSeconds()).slice(-2);
+           	var data = {
+           		Fix: {
+           			vehicle_id:1,
+           			latitude:$scope.map.marker.coords.latitude,
+           			longitude: $scope.map.marker.coords.longitude,
+           			fix_time: myDate,
+
+           			
+           		}
+           	};
+			http.post(url,data);
+           };
+       
+      $scope.pause = function() {
+      	$scope.paused = !$scope.paused;
+      };
+
+uiGmapIsReady.promise(1).then(function(instances) {
+     var origin1 = new google.maps.LatLng(18.487826949218416,-69.96467035609737);
+    var destinationB = new google.maps.LatLng(18.4878797,-69.9631046);
+
+    var service = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+      {
+        origins: [origin1],
+        destinations: [destinationB],
+        travelMode: google.maps.TravelMode.DRIVING,
+        avoidHighways: false,
+        avoidTolls: false
+      }, callback);
+
+    function callback(response, status) {
+    $scope.$apply(function(){
+        var row = response.rows.shift();
+        var element = row.elements.shift();
+        if (element.status == "OK") {
+          var distance = element.distance.text;
+          var duration = element.duration.text;
+          $scope.distancia = distance;
+          $scope.duracion = duration;
+        } else {
+          console.log("No se  pudo calcular la ruta.")
+        }
+        
+    })};
+    });
+
+		
 	}]);
